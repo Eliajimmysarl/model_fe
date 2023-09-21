9b85d69203502d5416bf60c809e23adae9016e7c
Jimmy ELIA <jimmy.elia@ejsarl.com>
2023-09-21 01:03:13 -0700

    update ajout plusieurs

--- home/composant/ajout_plusieurs/model/ajout_plusieurs.php
+++ home/composant/ajout_plusieurs/model/ajout_plusieurs.php
@@ -1,47 +1,38 @@
-
-
 <?php
-$uri =  $authority.'/texte/';
-//Recuperer les variables POST
-
-$text=$_POST['text'];
-$select=$_POST['select'];
-$email=$_POST['email'];
-$date=$_POST['date'];
-$password=$_POST['password'];
-$optionsRadios=$_POST['optionsRadios'];
-
-
-
-$data = array(
     
-    
-    'text' => $text,
-    'select'=> $select,
-    'email'=> $email,
-    'date'=> $date,
-    'password'=> $password,
-    'optionsRadios'=> $optionsRadios,
-   
-);
-$result=curl_post($uri, $token, $data);
-$textes=json_decode($result);
+    $uri =  $authority.'/texte/';
 
-$code = $textes->code;
+    $nombreLigne=$_POST['nombre_ligne'];
+
+    $test=array();
+
+    $select=array();
+
+    for($i=0; $i < $nombreLigne; ++$i )
+        {
+            $test['test_'.$i]= $_POST['test_'.$i];
+
+            $select['test_'.$i]= $_POST['select_'.$i];
+        }
+
+    $data = array();
+
+    $data['test'][] = $test;
+
+    $data['select'][] = $test;
+
+    $result=curl_post($uri, $token, $data);
+
+    $textes=json_decode($result);
+
+    $code = $textes->code;
     
-if($code ==201)
+    if($code ==201)
         {   
-           
-            
-            //Intregration de l'IHM affichant la reponse positive
             require_once('interactions/texte/ajouter/view/reponse_positive.php'); 
         }
     else    
         {
-            
-            //Intregration de l'IHM affichant la reponse negative
             require_once('interactions/texte/ajouter/view/reponse_negative.php');   
         }
-
-
 ?>
